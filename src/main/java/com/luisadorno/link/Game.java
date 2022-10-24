762f816c78f2ed9967dcfe61db4b17c4d23645a1
Luis Adorno <luis.adorno5@upr.edu>
2022-10-24 16:45:16 -0400

    Added and configured Game window.

---
+++ src/main/java/com/luisadorno/link/Game.java
@@ -0,0 +1,65 @@
+package com.luisadorno.link;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.Serial;
+
+public class Game extends Canvas implements Runnable{
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public static int width = 300;
+    public static int height = width / 16 * 9;
+    public static int scale = 3;
+
+    private Thread thread;
+    private JFrame frame;
+    private boolean running = false;
+
+    public Game(){
+        Dimension size = new Dimension(width * scale, height * scale);
+        setPreferredSize(size);
+        frame = new JFrame();
+    }
+
+    public synchronized void start(){
+        running = true;
+        thread = new Thread(this, "Display");
+        thread.start();
+    }
+
+    public synchronized void stop(){
+        running = false;
+        try{
+            thread.join();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        while(running){
+            update();
+            render();
+        }
+    }
+
+    public void update(){
+    }
+
+    public void render(){
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.frame.setResizable(false);
+        game.frame.setTitle("Link");
+        game.frame.add(game);
+        game.frame.pack();
+        game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        game.frame.setLocationRelativeTo(null);
+        game.frame.setVisible(true);
+        game.start();
+    }
+}
